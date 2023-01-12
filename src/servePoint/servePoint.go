ba4823fe89e7aed1f5753ad18c79a3b7c8dcf4fb
Aurutus <emslhy@hotmail.com>
2023-01-12 12:55:31 +0800

    Fix multi-line comment

--- src/servePoint/servePoint.go
+++ src/servePoint/servePoint.go
@@ -4,7 +4,6 @@ package servepoint
 	ServerPoint is an entrance which acts as a service integration point.
 	It's implemented as the client or the server depending on protocals and scinarios;
 */
-
 type ServerPoint interface {
 	Serve()
 }
