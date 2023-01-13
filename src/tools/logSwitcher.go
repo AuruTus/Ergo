3cbafd59e7e24802fe2239db1f752c886c916a71
Aurutus <emslhy@hotmail.com>
2023-01-13 12:27:45 +0800

    Fix check rule for config map keys

--- src/tools/logSwitcher.go
+++ src/tools/logSwitcher.go
@@ -3,6 +3,7 @@ package tools
 import (
 	"os"
 	"reflect"
+	"unicode"
 
 	"github.com/sirupsen/logrus"
 )
@@ -47,19 +48,54 @@ func logSwitcher() *logrus.Logger {
 
 type LogConfigs map[string]any
 
+func isLogConfigKeyValid(key string) bool {
+	isUpperASCIILetter := func(r rune) bool {
+		return r < unicode.MaxASCII && unicode.IsUpper(r)
+	}
+
+	isValidIdentifierName := func(s string) bool {
+		for _, r := range s {
+			if r > unicode.MaxASCII || !unicode.IsLetter(r) && r != '_' {
+				return false
+			}
+		}
+		return true
+	}
+
+	isExportedField := func(key string) bool {
+		return isUpperASCIILetter([]rune(key[:1])[0]) &&
+			isValidIdentifierName(key)
+	}
+
+	return isExportedField(key)
+}
+
 /* NewConfiguredLog works for server points */
 func NewConfiguredLogger(configs LogConfigs) (logger *logrus.Logger) {
 	logger = logSwitcher()
-	loggerRef := reflect.ValueOf(logger)
+	loggerRef := reflect.ValueOf(logger).Elem()
 
 	for fieldName, config := range configs {
+		/*
+			NOTE: unicode package will treat excluding letters as Lower or Upper Letters.
+			To avoid these undesired rule check, we need define our own ASCII identifier name
+			checker
+		*/
+		if !isLogConfigKeyValid(fieldName) {
+			Log.WithFields(logrus.Fields{"fieldName": fieldName}).
+				Warnf("Invalid config fieldName\n")
+			continue
+		}
+
 		configType := reflect.TypeOf(config)
 		configVal := reflect.ValueOf(config)
 
 		field := loggerRef.FieldByName(fieldName)
 		// if there's invalid config in arguments, neglect it and continue.
-		if !field.IsValid() || field.Type() != configType {
-			Log.WithFields(map[string]any{"fieldName": fieldName, "configValue": config}).Errorf("Invalid log config entry\n")
+		if !field.IsValid() || !field.CanSet() || (!field.CanConvert(configType) && !configType.ConvertibleTo(field.Type())) {
+			// the field.Type() equals to the type in the struct definition. It maybe an interface.
+			Log.WithFields(logrus.Fields{"fieldName": fieldName, "configType": configType}).
+				Warnf("Invalid log config entry\n")
 			continue
 		}
 
