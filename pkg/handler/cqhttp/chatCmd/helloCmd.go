0580381e41a3bae927ff16c507205296e3bedd7b
Aurutus <emslhy@hotmail.com>
2023-01-25 23:10:00 +0800

    Add hello and help cmd

---
+++ pkg/handler/cqhttp/chatCmd/helloCmd.go
@@ -0,0 +1,23 @@
+package chatcmd
+
+func init() {
+	OnCommand(helloHandle, helloDesc, helloInfo, "hello")
+}
+
+const (
+	helloDesc = `"hello" is for comunication test`
+	helloInfo = `
+	===============================================
+	  ______     ______     ______     ______    
+	  /\  ___\   /\  == \   /\  ___\   /\  __ \   
+	  \ \  __\   \ \  __<   \ \ \__ \  \ \ \/\ \  
+	   \ \_____\  \ \_\ \_\  \ \_____\  \ \_____\ 
+	    \/_____/   \/_/ /_/   \/_____/   \/_____/ 
+												
+	===============================================
+	`
+)
+
+func helloHandle(c *cmdNode) string {
+	return "Hello! This is Ergo"
+}
--- pkg/handler/cqhttp/chatCmd/helpCmd.go
+++ pkg/handler/cqhttp/chatCmd/helpCmd.go
@@ -4,35 +4,53 @@ func init() {
 	OnCommand(helpHandle, helpDesc, helpInfo, "h", "help")
 }
 
-var (
-	helpDesc = "`help` lists available commands on this bot"
-	helpInfo = `
-		-v --verbose: list the detailed information of available commands
-	`
+const (
+	helpDesc = `"help" lists available commands on this bot. Use "-v", "--verbose" to get more infomation.`
+	helpInfo = `-v --verbose: list the detailed information of available commands`
 )
 
-func helpHandle(c *cmdNode) []byte {
-	msg := make([]byte, 0, 128)
+var (
+	descLine = []byte("@ DESC> ")
+	infoLine = []byte("\n@ INFO-->\n")
+)
 
-	var (
-		verboseFlag = false
-	)
+func helpHandle(c *cmdNode) string {
+	msg := make([]byte, 0, 512)
 
+	verboseFlag := false
 	for _, o := range c.opts {
 		switch {
-		case o.opt == "-v" || o.opt == "--verbose":
+		case o.opt == "v" || o.opt == "verbose":
 			verboseFlag = true
 		}
 	}
 
-	for _, cmd := range commands {
-		cLine := []byte(cmd.desc)
-		if verboseFlag {
-			cLine = append(cLine, '\n')
-			cLine = append(cLine, []byte(cmd.info)...)
+	visited := make(map[*handleFunc]struct{})
+	for _, c := range commands {
+		if _, ok := visited[&c.handle]; ok {
+			continue
 		}
-		cLine = append(cLine, '\n')
-		msg = append(msg, cLine...)
+		visited[&c.handle] = struct{}{}
+
+		// get segment string like: "@ DESC-> .h .help: "
+		cmds := append([]byte{}, descLine...)
+		for _, name := range c.cmds {
+			cmds = append(cmds, '.')
+			cmds = append(cmds, []byte(name)...)
+			cmds = append(cmds, ' ')
+		}
+		cmds[len(cmds)-1] = ':'
+		cmds = append(cmds, ' ')
+
+		cmdBlock := []byte(c.desc)
+		if verboseFlag {
+			cmdBlock = append(cmdBlock, infoLine...)
+			cmdBlock = append(cmdBlock, []byte(c.info)...)
+		}
+		cmdBlock = append(cmdBlock, '\n')
+
+		msg = append(msg, cmds...)
+		msg = append(msg, cmdBlock...)
 	}
-	return msg
+	return string(msg)
 }
